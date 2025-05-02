dc0032c1c837b3ddd68e71a849abce9ebe0f0c21
ezekielmose <70557286+ezekielmose@users.noreply.github.com>
2025-05-02 21:00:39 +0300

    Update hd_system_web_app.py

--- hd_system_web_app.py
+++ hd_system_web_app.py
@@ -80,9 +80,9 @@ def main():
     ST_Depression = st.text_input("Patient's ST Depression [ECG or EKG] (0.0 - 4.4)")
     Slope_of_Excersize	 = st.text_input("Patient's Slope of Excersize (0,1 or 2)")
     Number_of_vessels = st.text_input("Number of vessels (0, 1,2,3 or 4)")
-    Thalassemia = st.text_input("Thalassemia (1,2,3 or 4)")
 
-     Thalassemia = st.number_input( "Thalassemia Level", 
+    # INCREMENT/DECREAMENT OPTIONS
+    Thalassemia = st.number_input( "Thalassemia Level", 
         min_value=0, 
         max_value=3, 
         step=1
